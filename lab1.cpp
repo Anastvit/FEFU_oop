220d62d0e34642cd392d004a7e6a4807f7d74950
Anastvit <borovik29835@gmail.com>
2022-11-26 10:46:35 +1000

    first commit

---
+++ lab1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cmath>
+#include <limits>
+
+double func(double x, int n)
+{
+    double y = 0;
+
+    for (int k = 1; k <= n; k++)
+    {
+        y += (pow(x, (2 * k - 2)) / (2 * k - 1));
+    }
+    y += 0.5 * x;
+
+    return y;
+}
+
+int main()
+{
+    double x;
+    int n;
+
+    std::cout << "x=";
+    std::cin >> x;
+    if (std::cin.fail())
+    {
+        std::cout << "Invalid input." << std::endl << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return 0;
+    }
+
+    std::cout << "n=";
+    std::cin >> n;
+    if (std::cin.fail())
+    {
+        std::cout << "Invalid input." << std::endl << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return 0;
+    }
+
+    std::cout << "function value=" << func(x, n) << std::endl;
+
+    return 0;
+}
---
+++ lab2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <ctime>
+
+int main()
+{
+    int N = 7;
+    int M = 10;
+
+    int** matrix = new int* [N];
+    for (int i = 0; i < N; i++)
+        matrix[i] = new int[M];
+
+    srand(static_cast<unsigned int>(time(NULL)));
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            matrix[i][j] = 1 + rand() % 101;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            std::cout.width(5);
+            std::cout << matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i; j < N; j++)
+        {
+            if (matrix[j][0] < matrix[i][0])
+            {
+                int* tmp = matrix[j];
+                matrix[j] = matrix[i];
+                matrix[i] = tmp;
+            }
+        }
+    }
+
+    std::cout << std::endl << std::endl;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            std::cout.width(5);
+            std::cout << matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < N; i++)
+        delete[] matrix[i];
+    delete[] matrix;
+}
